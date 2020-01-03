89cae6b8cb792c9c1f40a79a3645ca2a280c292c
hashrock <hashrock@users.noreply.github.com>
2020-01-03 23:28:51 +0900

    fix: wait until timeline end

--- index.browser.js
+++ index.browser.js
@@ -3,14 +3,20 @@ function timelineDown(el, str, frames, options) {
   var frameDuration =
     options && options.frameDuration ? options.frameDuration : 0.1;
   var time = 0;
+  var totalTime = frameDuration * str.length;
   for (var i = 0; i < str.length; i++) {
     var ch = str[i];
     if (ch.match(/[0-9]/)) {
       var frameIndex = parseInt(ch, 10);
       tl.to(el, time, frames[frameIndex]);
+      totalTime -= time;
       time = 0;
     }
     time += frameDuration;
   }
+  //Wait until timeline end
+  const last = { ...frames[str.length - 1] };
+  tl.to(el, totalTime, last);
+
   return tl;
 }
--- index.js
+++ index.js
@@ -5,14 +5,20 @@ module.exports = function timelineDown(el, str, frames, options) {
   const frameDuration =
     options && options.frameDuration ? options.frameDuration : 0.1;
   let time = 0;
+  var totalTime = frameDuration * str.length;
   for (let i = 0; i < str.length; i++) {
     const ch = str[i];
     if (ch.match(/[0-9]/)) {
       const frameIndex = parseInt(ch, 10);
       tl.to(el, time, frames[frameIndex]);
+      totalTime -= time;
       time = 0;
     }
     time += frameDuration;
   }
+  //Wait until timeline end
+  const last = { ...frames[str.length - 1] };
+  tl.to(el, totalTime, last);
+
   return tl;
 };
